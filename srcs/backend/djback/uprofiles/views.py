7cfa4cbc16441ac5f1f767e9b147decf368686f7
gbreana <edviniii@gmail.com>
2025-03-04 17:39:26 +0100

    experiments with APIView

--- srcs/backend/djback/uprofiles/views.py
+++ srcs/backend/djback/uprofiles/views.py
@@ -1,7 +1,23 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
+
 from .models import User, UserProfile
 from .serializers import UserProfileSerializer
 
-class UserProfileAPIView(generics.ListAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+# class UserProfileAPIView(generics.ListAPIView):
+#     queryset = UserProfile.objects.all()
+#     serializer_class = UserProfileSerializer
+
+# class UserProfileAPIView(APIView):
+#     def get(self, request):
+#         return Response({'title': 'SomeOne'})
+
+#     def post(self, request):
+#         profile_new = UserProfile.objects.create(
+#             user = None,
+#             avatar = request.data['avatar'],
+#             bio = request.data['bio'],
+#         )
+#         return Response({'bio': model_to_dict(profile_new)})
