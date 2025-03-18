a11832f8a01a87c239117e611f77a2141a4f6b13
edvin3i <edviniii@gmail.com>
2025-03-18 17:51:56 +0100

    42serializer create method

--- srcs/backend/transa/trans_auth/serializers.py
+++ srcs/backend/transa/trans_auth/serializers.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import request
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -17,10 +18,54 @@ class FortyTwoAuthSerializer(serializers.Serializer):
     refresh_token = serializers.CharField(read_only=True)
     username = serializers.CharField(read_only=True)
 
-    def validate_code(self, request):
+    def validate_code(self, code):
         if not any(OA_CLIENT_ID or OA_SECRET or OA_REDIR_URL):
-            raise serializers.ValidationError("Client ID or Secret was not provided")
+            raise serializers.ValidationError("OAuth config params was not provided")
 
+        token_url = OA_TOKEN_URL
 
+        data = {
+            "client_id": OA_CLIENT_ID,
+            "client_secret": OA_SECRET,
+            "redirect_uri": OA_REDIR_URL,
+            "code": code,
+            "grant_type": "autorization_code",
+        }
 
+        resp = requests.post(token_url, data=data)
+
+        if resp.status_code != 200:
+            raise serializers.ValidationError("Failed exchange the code for token")
+
+        token_info = resp.json()
+        access_token = token_info.get("access_token")
+        if not access_token:
+            raise serializers.ValidationError("No access token returned from 42")
+
+        userinfo_url = "https://api.intra.42.fr/v2/me"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        resp_user = requests.get(userinfo_url, headers=headers)
+        if resp_user.status_code != 200:
+            raise serializers.ValidationError("Failed get user data from 42")
+        user_data = resp_user.json()
+
+        self.context["42_access_token"] = access_token
+        self.context["42_user_data"] = user_data
+
+    def create(self, validated_data):
+        user_data = self.context["42_user_data"]
+        ft_login = user_data.get("login")
+        ft_email = user_data.get("email")
+        ft_fname = user_data.get("first_name")
+        ft_lname = user_data.get("last_name")
+        # ft_avatar = user_data.get("image").get("link")
+
+        user, _created = User.objects.get_or_create(
+            username=ft_login,
+            email=ft_email,
+            first_name=ft_fname,
+            last_name=ft_lname,
+        )
+
+        refresh = RefreshToken.for_user(user)
 
