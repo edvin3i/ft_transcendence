1d43ae4b674d74f62187eaac96c09cbe06e38652
edvin3i <edviniii@gmail.com>
2025-03-19 13:50:18 +0100

    remove DOT

--- srcs/backend/transa/trans_auth/urls.py
+++ srcs/backend/transa/trans_auth/urls.py
@@ -7,9 +7,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('ft/callback/', FortyTwoCallbackView.as_view(), name='ft_callback'),
-
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("ft/callback/", FortyTwoCallbackView.as_view(), name="ft_callback"),
 ]
--- srcs/backend/transa/transa/configs/base.py
+++ srcs/backend/transa/transa/configs/base.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     # "rest_framework.authtoken",
-    'rest_framework_simplejwt',
+    "rest_framework_simplejwt",
     "uprofiles",
     "trans_auth",
     "game",
@@ -130,7 +130,7 @@ REST_FRAMEWORK = dict()
 REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = [
     "rest_framework.authentication.BasicAuthentication",
     "rest_framework.authentication.SessionAuthentication",
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "rest_framework_simplejwt.authentication.JWTAuthentication",
 ]
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
     "rest_framework.renderers.JSONRenderer",
@@ -144,7 +144,6 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
-
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
     "VERIFYING_KEY": "",
@@ -153,23 +152,18 @@ SIMPLE_JWT = {
     "JSON_ENCODER": None,
     "JWK_URL": None,
     "LEEWAY": 0,
-
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
     "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
-
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
-
     "JTI_CLAIM": "jti",
-
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-
     "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
     "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
@@ -185,4 +179,4 @@ OA_TOKEN_URL = os.getenv("OA_TOKEN_URL", "https://api.intra.42.fr/oauth/token")
 OA_CLIENT_ID = os.getenv("OA_CLIENT_ID", None)
 OA_SECRET = os.getenv("OA_SECRET", None)
 OA_REDIR_URL = os.getenv("OA_REDIR_URL", "api/auth/ft/callback/")
-OA_POST_LOUT_REDIR_URLS = os.getenv("OA_POST_LOUT_REDIR_URLS", "/")
+OA_POST_LOUT_REDIR_URL = os.getenv("OA_POST_LOUT_REDIR_URL", "/")
