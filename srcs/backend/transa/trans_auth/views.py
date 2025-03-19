4b7cdf1773cb0c505914585fed06fd4c31c7d893
edvin3i <edviniii@gmail.com>
2025-03-19 13:50:18 +0100

    remove DOT

--- srcs/backend/transa/trans_auth/views.py
+++ srcs/backend/transa/trans_auth/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FortyTwoAuthSerializer
 
+
 class FortyTwoCallbackView(GenericAPIView):
     serializer_class = FortyTwoAuthSerializer
 
