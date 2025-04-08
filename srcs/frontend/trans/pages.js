94e2fcd87d5eb4da825d17287d7949183f76ae11
Paul KORSAKOFF <pkorsako@paul-f3Br6s6.clusters.42paris.fr>
2025-04-08 19:10:29 +0200

    first visual chcanges, style.css added

--- srcs/frontend/trans/pages.js
+++ srcs/frontend/trans/pages.js
@@ -1,14 +1,51 @@
+// function logInPage()
+// {
+// 	return `
+// 		<p>Please, log in to play the game.</p>
+// 		<button id="logInWith42Button">Log in with 42</button>
+// 		<p>---or---</p>
+// 		<form id="logInForm" method="POST">
+// 			<div>
+// 				<label for="username">Enter your username:</label>
+// 				<input  type="username"
+// 						id="username"
+// 						placeholder="username"
+// 						minlength="3"
+// 						maxlength="20"
+// 						pattern="[a-zA-Z0-9_]+"
+// 						title="Username can only contain letters, numbers, and underscores, and must be 3-20 characters long."
+// 						required/>
+// 			</div>
+// 			<div>
+// 				<label for="password">Enter your password:</label>
+// 				<input type="password"
+// 						id="password"
+// 						placeholder="********"
+// 						required/>
+// 			</div>
+// 			<button type="submit">Log in</button>
+// 		</form>
+// 		<p id="logInResult"></p>
+// 		<div>
+// 			<p style="display: inline;">Don't have an account?</p>
+// 			<button style="display: inline;" id="createAccountButton">Create account</button>
+// 		</div>`;
+// }
+
 function logInPage()
 {
 	return `
-		<p>Please, log in to play the game.</p>
-		<button id="logInWith42Button">Log in with 42</button>
-		<p>---or---</p>
-		<form id="logInForm" method="POST">
-			<div>
-				<label for="username">Enter your username:</label>
+		<p class="text-center">Please, log in to play the game.</p>
+		<div class="text-center">
+			<button id="logInWith42Button" class="btn btn-dark mb-3">Log in with 42</button>
+		</div>
+		<p class="text-center">---or---</p>
+		<form id="logInForm" method="POST" class="container">
+			<div class="mb-3">
+				<label for="username" class="form-label">Enter your username:</label>
 				<input  type="username"
 						id="username"
+						class="form-control"
 						placeholder="username"
 						minlength="3"
 						maxlength="20"
@@ -16,27 +53,28 @@ function logInPage()
 						title="Username can only contain letters, numbers, and underscores, and must be 3-20 characters long."
 						required/>
 			</div>
-			<div>
-				<label for="password">Enter your password:</label>
+			<div class="mb-3">
+				<label for="password" class="form-label">Enter your password:</label>
 				<input type="password"
 						id="password"
+						class="form-control"
 						placeholder="********"
 						required/>
 			</div>
-			<button type="submit">Log in</button>
+			<button type="submit" class="btn btn-primary w-100">Log in</button>
 		</form>
-		<p id="logInResult"></p>
-		<div>
+		<p id="logInResult" class="text-danger mt-3 text-center"></p>
+		<div class="text-center mt-3">
 			<p style="display: inline;">Don't have an account?</p>
-			<button style="display: inline;" id="createAccountButton">Create account</button>
+			<button style="display: inline;" id="createAccountButton" class="btn btn-link">Create account</button>
 		</div>`;
 }
-			
+
 function accountCreationPage()
 {
 	return `
 		<div id="accountCreationHeader">
-			<p>Please, create your account to play the game.</p>
+			<p class="text-center">Please, create your account to play the game.</p>
 			<button id="signUpWith42Button">Sign up with 42</button>
 			<p>---or---</p>
 		</div>
