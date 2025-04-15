84ba64aeaf30057bea37b3227733d1cf0806983c
edvin3i <edviniii@gmail.com>
2025-04-15 14:01:08 +0200

    add urls and views protos in friends app

--- srcs/backend/transa/friends/views.py
+++ srcs/backend/transa/friends/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+
+class FriendsCreateRequestAPIView(generics.CreateAPIView):
+    pass
+
+
+class FriendsDeleteAPIView(generics.DestroyAPIView):
+    pass
+
+
+class FriendsRequestAcceptAPIView(generics.UpdateAPIView):
+    pass
+
+
+class FriendsRequestRejectAPIView(generics.UpdateAPIView):
+    pass
+
+
+class FriendRequestsIncomingListAPIView(generics.ListAPIView):
+    """
+    incoming
+    """
+
+    pass
+
+
+class FriendRequestsOutgoingListAPIView(generics.ListAPIView):
+    """
+    outgoing
+    """
+
+    pass
+
+
+class FriendsBlockAPIView(generics.UpdateAPIView):
+    pass
+
+
+class FriendsUnblockAPIView(generics.UpdateAPIView):
+    pass
+
+
+class FriendsAllListAPIView(generics.ListAPIView):
+    """
+    all
+    """
+
+    pass
+
+
+class FriendsOnlineListAPIView(generics.ListAPIView):
+    """
+    online
+    """
+
+    pass
+
+
+class FriendsBlockedListAPIView(generics.ListAPIView):
+    """
+    blocked
+    """
+
+    pass
