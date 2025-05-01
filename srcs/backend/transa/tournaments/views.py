11fb18e7475e3c9191323d098d3eee6e1d53412a
edvin3i <edviniii@gmail.com>
2025-05-01 22:56:53 +0200

    add tour join view

--- srcs/backend/transa/tournaments/views.py
+++ srcs/backend/transa/tournaments/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .models import Tournament
-from .serializers import TournamentSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_list_or_404
+from tournaments.models import Tournament, TournamentParticipant
+from tournaments.serializers import TournamentSerializer
+from tournaments.service import generate_tour_bracket
+from game.serializers import MatchSerializer
+from game.models import Match
 
 
 class TournamentsListAPIView(generics.ListAPIView):
@@ -35,3 +41,30 @@ class TournamentUpdateAPIView(generics.UpdateAPIView):
     queryset = Tournament.objects.all()
     serializer_class = TournamentSerializer
     permission_classes = [IsAuthenticated]
+
+
+class TournamentJoinAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        tour: Tournament = get_list_or_404(Tournament, pk=pk, is_started=False)
+        if tour.current_players_count >= tour.max_players:
+            return Response(
+                {'detail': "Tournament is full."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        obj, created = TournamentParticipant.objects.get_or_create(
+            tournament=tour, player=request.user.profile
+        )
+
+        if not created:
+            return Response(
+                {'detail': "You are registered already."},
+                status=status.HTTP_200_OK,
+            )
+        tour.current_players_count += 1
+        tour.save(update_fields=['current_players_count'])
+        return Response(
+                {'detail': "You are rigistered succefully!"},
+                status=status.HTTP_201_CREATED,
+            )
