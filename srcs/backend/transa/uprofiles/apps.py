efed0f60bcfcc4d6d5c962ad234f6198b0bb50e5
edvin3i <edviniii@gmail.com>
2025-05-04 12:41:02 +0200

    add user profile for admin

--- srcs/backend/transa/uprofiles/apps.py
+++ srcs/backend/transa/uprofiles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UprofilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "uprofiles"
+
+    def ready(self):
+        import uprofiles.signals
